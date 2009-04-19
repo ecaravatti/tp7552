544df5f14afec7fa0a47f5733ffaa823d8dfd190
manugarciacab <manugarciacab@ff53ae6e-1c7a-11de-b6aa-3535bc3264a2>
2009-04-19 20:04:33 +0000

    Implementacion inicial y rapida de arboles

---
+++ trunk/tests/arbol/ArbolTest.java
@@ -0,0 +1,27 @@
+package arbol;
+
+import arbol.altura.BSTree;
+import arbol.altura.BTData;
+import junit.framework.TestCase;
+
+public class ArbolTest extends TestCase {
+	
+	public void treeTest() {
+		BSTree tree = new BSTree();
+		
+		insertar(tree, "10");
+		encontrar(tree, "10");
+		encontrar(tree, "09");
+		encontrar(tree, "11");
+		encontrar(tree, "10");
+	}
+	
+	private void insertar(BSTree tree, String valor) {
+		System.out.println(valor + (tree.insertAVL(new BTData(valor)) != null ? " insertado correctamente" : " no fue insertado"));
+	}
+	
+	private void encontrar(BSTree tree, String valor) {
+		System.out.println((tree.locate(new BTData(valor)) != null ? "Encontré" : "No encontré") + " el " + valor);
+	}
+
+}
