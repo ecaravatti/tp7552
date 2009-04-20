260096a7c9c3fe8f169d2609e2cba67d63c453d6
rdz.maria.laura <rdz.maria.laura@ff53ae6e-1c7a-11de-b6aa-3535bc3264a2>
2009-04-20 00:01:34 +0000

    Versión inicial del stack.

---
+++ trunk/src/java/stack/MyStack.java
@@ -0,0 +1,52 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class MyStack<T> {
+
+	private final static int DEFAULT_CAPACITY = 8;
+	private int top = -1;
+	private int capacity;
+	private ArrayList<T> stack;
+	
+	public MyStack() {
+		stack = new ArrayList<T>(DEFAULT_CAPACITY);
+	}
+	
+	public MyStack(int capacity){
+		this.capacity = capacity;
+		stack = new ArrayList<T>(capacity);
+	}
+
+	public void push(T element) throws Exception{
+		if(isFull()){
+			throw new Exception();
+		}
+		else{
+			stack.add(element);
+			top++;
+		}		
+	}
+	
+	public T pop() throws EmptyStackException{
+		if(isEmpty()){
+			throw new EmptyStackException();
+		}
+		else{		
+			T element = stack.get(top);
+			stack.remove(top);
+			top--;
+			
+			return element;
+		}
+	}
+	
+	private boolean isEmpty(){
+		return(top == -1);
+	}
+	
+	private boolean isFull(){
+		return(top == capacity);
+	}
+}
