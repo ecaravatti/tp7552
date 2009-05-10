8d03d0f22744940446e6c9cb61ddb4a6b3599c98
manugarciacab <manugarciacab@ff53ae6e-1c7a-11de-b6aa-3535bc3264a2>
2009-05-10 21:57:15 +0000

    Test de arbol de altura

--- trunk/tests/tree/HeightBTreeTest.java
+++ trunk/tests/tree/HeightBTreeTest.java
@@ -1,6 +1,7 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import tree.binary.BTNode;
@@ -11,84 +12,59 @@ import junit.framework.TestCase;
 
 public class HeightBTreeTest extends TestCase {
 	
-	public void testHeightBTree() {
-		BTree tree = new HeightBTree(4);
+	public void testHeightTreeBehavior() {
+		for (int i = 1; i < 5; i++) {
+			testHeightTree(i, 10000);
+		}
+	}
+	
+	private void testHeightTree(int heightVariation, int insertsAmount) {
+		BTree tree = new HeightBTree(heightVariation);
 		
 		List<Integer> insertados = new ArrayList<Integer>();
-	/*	insertados.add(498);
-		insertados.add(521);
-		insertados.add(368);
-		insertados.add(843);
-		insertados.add(975);
-		insertados.add(731);
-		insertados.add(671);
-		insertados.add(777);
-		insertados.add(241);
-		insertados.add(174);*/
-	/*	insertados.add(5);
-		insertados.add(10);
-		insertados.add(74);
-		insertados.add(93);
-		insertados.add(86);*/
-		for (Integer codigo : insertados) {
-			insertar(tree, codigo);
-		}
-		for (int i = 0; i < 1000; i++) {
-			int random = (int)(Math.random()*99999);
-			insertar(tree, random);
-			insertados.add(random);
-		}
-		int i = 0;
-		for (Integer codigo : insertados) {
-			if (i++ < 980) borrar(tree, codigo);
-		}
-		System.out.println(tree);
-	}
-	
-	private void insertar(BTree tree, int valor) {
-		System.out.println(valor + (tree.insert(new BTData(valor)) != null ? " insertado correctamente" : " no fue insertado"));
-	}
-	
-	private void encontrar(BTree tree, int valor) {
-		System.out.println((tree.locate(new BTData(valor)) != null ? "Encontré" : "No encontré") + " el " + valor);
-	}
-	
-	private void borrar(BTree tree, int valor) {
-		tree.delete(new BTData(valor), 1);
-		System.out.println("Borro el " + valor + " si existe");
-	}
-
-	public void testBalances() {
-		BTree tree = new HeightBTree(3);
-		
-		List<Integer> insertados = new ArrayList<Integer>();
-		for (int i = 0; i < 10000; i++) {
-			int random = (int)(Math.random()*999999);
+		for (int i = 0; i < insertsAmount; i++) {
+			int random = (int)(Math.random()*(insertsAmount*1000));
 			tree.insert(new BTData(random));
 			insertados.add(random);
 		}
+		
+		Collections.shuffle(insertados); //Los mezclo para que varíe el orden
 		BTNode node;
-		//System.out.println(tree);
 		for (Integer codigo : insertados) {
 			node = tree.locate(new BTData(codigo));
-		//	System.out.println(node);
-			assertEquals(node.getBalance(), node.getBalanceTeorico());
+			assertNotNull(node); //Lo encontré
+			assertEquals(node.getBalance(), node.getBalanceTeorico()); //El balance está bien calculado
+			assertTrue(Math.abs(node.getBalance()) <= heightVariation); //No está desbalanceado
 		}
 		
+		List<Integer> borrados = new ArrayList<Integer>();
+		Collections.shuffle(insertados);
 		int i = 0;
+		//Borro menos de los que inserté
 		for (Integer codigo : insertados) {
-			if (i++ < 9980) tree.delete(new BTData(codigo), 1);
-		}
-		
-		for (Integer codigo : insertados) {
-			node = tree.locate(new BTData(codigo));
-			if (node != null) {
-		//		System.out.println(node);
-				assertEquals(node.getBalance(), node.getBalanceTeorico());
+			if (i++ < (insertsAmount*4/5)) {
+				borrados.add(codigo);
+				tree.delete(new BTData(codigo), 1);
+			} else {
+				break;
 			}
 		}
 		
-	//	System.out.println(tree);
+		for (Integer codigo : borrados) {
+			node = tree.locate(new BTData(codigo));
+			assertNull(node); //Efectivamente está borrado
+		}
+		
+		for (Integer codigo : insertados) {
+			node = tree.locate(new BTData(codigo));
+			if (borrados.contains(codigo)) {
+				assertNull(node); //Efectivamente está borrado
+			} else {
+				assertNotNull(node); //Lo encontré
+				assertEquals(node.getBalance(), node.getBalanceTeorico()); //El balance está bien calculado
+				assertTrue(Math.abs(node.getBalance()) <= heightVariation); //No está desbalanceado
+			}
+		}
 		
 	}
 
