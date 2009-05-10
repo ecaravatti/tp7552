d5971d996a899e5eee6be2cf3788141d33bfce78
ecaravatti <ecaravatti@ff53ae6e-1c7a-11de-b6aa-3535bc3264a2>
2009-05-10 19:57:38 +0000

    hay home page

--- trunk/src/java/common/web/HomePage.java
+++ trunk/src/java/common/web/HomePage.java
@@ -4,43 +4,6 @@ package common.web;
 import org.apache.wicket.markup.html.WebPage;
 
 
-/**
- * Body frame page for the frames example.
- * 
- * @author Javier Persico
- */
-public class HomePage extends WebPage
-{
+public class HomePage extends WebPage {
 
-    /** name for page map etc. */
-    public static final String RIGHT_FRAME_NAME = "right";
-
-    //private final FrameTarget frameTarget = new FrameTarget(PaginaPrueba.class);
-
-    /**
-     * Constructor
-     */
-    public HomePage()
-    {
-
-    }
-
-    /**
-     * Gets frameTarget.
-     * 
-     * @return frameTarget
-     */
-    /*public FrameTarget getFrameTarget()
-    {
-        return frameTarget;
-    }*/
-
-    /**
-     * @see org.apache.wicket.Component#isVersioned()
-     */
-    @Override
-    public boolean isVersioned()
-    {
-        return false;
-    }
 }
