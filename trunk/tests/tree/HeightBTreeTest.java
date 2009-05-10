622260ec9e4c4c5258a31750bc46d9a1c63bc482
manugarciacab <manugarciacab@ff53ae6e-1c7a-11de-b6aa-3535bc3264a2>
2009-05-10 17:02:09 +0000

    Test

--- trunk/tests/tree/HeightBTreeTest.java
+++ trunk/tests/tree/HeightBTreeTest.java
@@ -1,5 +1,9 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tree.binary.BTNode;
 import tree.binary.BTree;
 import tree.binary.BTData;
 import tree.binary.height.HeightBTree;
@@ -8,22 +12,37 @@ import junit.framework.TestCase;
 public class HeightBTreeTest extends TestCase {
 	
 	public void testHeightBTree() {
-		BTree tree = new HeightBTree();
+		BTree tree = new HeightBTree(4);
 		
-		insertar(tree, 10);
-		encontrar(tree, 10);
-		insertar(tree, 4);
-		insertar(tree, 7);
-		encontrar(tree, 9);
-		encontrar(tree, 4);
-		insertar(tree, 10);
-		encontrar(tree, 10);
-		borrar(tree, 10);
-		encontrar(tree, 7);
-		borrar(tree, 7);
-		encontrar(tree, 7);
-		borrar(tree, 6);
-		encontrar(tree, 10);
+		List<Integer> insertados = new ArrayList<Integer>();
+	/*	insertados.add(498);
+		insertados.add(521);
+		insertados.add(368);
+		insertados.add(843);
+		insertados.add(975);
+		insertados.add(731);
+		insertados.add(671);
+		insertados.add(777);
+		insertados.add(241);
+		insertados.add(174);*/
+	/*	insertados.add(5);
+		insertados.add(10);
+		insertados.add(74);
+		insertados.add(93);
+		insertados.add(86);*/
+		for (Integer codigo : insertados) {
+			insertar(tree, codigo);
+		}
+		for (int i = 0; i < 1000; i++) {
+			int random = (int)(Math.random()*99999);
+			insertar(tree, random);
+			insertados.add(random);
+		}
+		int i = 0;
+		for (Integer codigo : insertados) {
+			if (i++ < 980) borrar(tree, codigo);
+		}
+		System.out.println(tree);
 	}
 	
 	private void insertar(BTree tree, int valor) {
@@ -37,6 +56,40 @@ public class HeightBTreeTest extends TestCase {
 	private void borrar(BTree tree, int valor) {
 		tree.delete(new BTData(valor), 1);
 		System.out.println("Borro el " + valor + " si existe");
+	}
+
+	public void testBalances() {
+		BTree tree = new HeightBTree(9);
+		
+		List<Integer> insertados = new ArrayList<Integer>();
+		for (int i = 0; i < 10000; i++) {
+			int random = (int)(Math.random()*999999);
+			tree.insert(new BTData(random));
+			insertados.add(random);
+		}
+		BTNode node;
+		//System.out.println(tree);
+		for (Integer codigo : insertados) {
+			node = tree.locate(new BTData(codigo));
+		//	System.out.println(node);
+			assertEquals(node.getBalance(), node.getBalanceTeorico());
+		}
+		
+		int i = 0;
+		for (Integer codigo : insertados) {
+			if (i++ < 9995) tree.delete(new BTData(codigo), 1);
+		}
+		
+		for (Integer codigo : insertados) {
+			node = tree.locate(new BTData(codigo));
+			if (node != null) {
+		//		System.out.println(node);
+				assertEquals(node.getBalance(), node.getBalanceTeorico());
+			}
+		}
+		
+		System.out.println(tree);
+		
 	}
 
 }
