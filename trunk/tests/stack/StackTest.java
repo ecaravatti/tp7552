9cf4ff7f7dd49e2c21aac8e1d9ff6546615a610d
rdz.maria.laura <rdz.maria.laura@ff53ae6e-1c7a-11de-b6aa-3535bc3264a2>
2009-05-15 02:54:50 +0000

    Agregué los printCommands.

--- trunk/tests/stack/StackTest.java
+++ trunk/tests/stack/StackTest.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import collection.stack.Stack;
 import command.Command;
@@ -22,6 +24,17 @@ public class StackTest extends TestCase {
 		stack.push(new Integer(3));
 		stack.push(new Integer(4));
 		stack.push(new Integer(5));
+	}
+
+	private List<Command> fillStackAndListCommands() {
+		List<Command> commandList = new ArrayList<Command>();
+		commandList.addAll(stack.push(new Integer(1)));
+		commandList.addAll(stack.push(new Integer(2)));
+		commandList.addAll(stack.push(new Integer(3)));
+		commandList.addAll(stack.push(new Integer(4)));
+		commandList.addAll(stack.push(new Integer(5)));
+		
+		return commandList;
 	}
 
 	public void testOverflow() {
@@ -90,6 +103,72 @@ public class StackTest extends TestCase {
 		// The stack is empty
 		commandList = stack.pop();
 		assertEquals(0, commandList.size());
+	}
+
+	private void printCommands(List<Command> commands, String description) {
+		System.out.println("---" + description + "---");
+		System.out.println("<EXECUTE>");
+		for (Command command : commands) {
+			System.out.println(command.execute());
+		}
+		System.out.println("<UNDO>");
+		Collections.reverse(commands);
+		for (Command command : commands) {
+			System.out.println(command.undo());
+		}
+		System.out.println("------FIN------\n");
+	}
+
+	public void testStackBehaviorForView() {
+		List<Command> commandList = new ArrayList<Command>();
+		printCommands(commandList, "PILA VACÍA");
+		
+		// The top of the stack is 1
+		printCommands(stack.push(new Integer(1)), "INSERTO EL PRIMER ELEMENTO EN LA PILA");
+		commandList = stack.peek();
+		
+		printCommands(commandList, "CONSULTO EL TOPE DE LA PILA");
+		assertEquals("1", ((HighlightCommand) commandList.get(0)).getContent());
+
+		// The top of the stack is 2
+		printCommands(stack.push(new Integer(2)), "INSERTO OTRO ELEMENTO EN LA PILA");
+		commandList = stack.peek();
+		
+		printCommands(commandList, "CONSULTO EL TOPE DE LA PILA");
+		assertEquals("2", ((HighlightCommand) commandList.get(0)).getContent());
+
+		// The top of the stack is 3
+		printCommands(stack.push(new Integer(3)), "INSERTO OTRO ELEMENTO EN LA PILA");
+		commandList = stack.peek();
+		
+		printCommands(commandList, "CONSULTO EL TOPE DE LA PILA");
+		assertEquals("3", ((HighlightCommand) commandList.get(0)).getContent());
+
+		// Pop 3
+		commandList = stack.pop();
+		printCommands(commandList, "QUITO UN ELEMENTO DEL TOPE DE LA PILA");
+		assertEquals(1, commandList.size());
+		assertEquals("3", ((PopCommand) commandList.get(0)).getContent());
+
+		// The top must be 2
+		commandList = stack.pop();
+		printCommands(commandList, "QUITO OTRO ELEMENTO DEL TOPE DE LA PILA");
+		assertEquals(1, commandList.size());
+		assertEquals("2", ((PopCommand) commandList.get(0)).getContent());
+
+		// The top must be 1
+		commandList = stack.pop();
+		printCommands(commandList, "QUITO OTRO ELEMENTO DEL TOPE DE LA PILA");
+		assertEquals(1, commandList.size());
+		assertEquals("1", ((PopCommand) commandList.get(0)).getContent());
+
+		// The stack is empty
+		commandList = stack.pop();
+		printCommands(commandList, "LA PILA ESTA VACÍA");
+		assertEquals(0, commandList.size());
+		
+		printCommands(fillStackAndListCommands(), "CARGO LA PILA CON 5 ELEMENTOS");
+		printCommands(stack.destroy(), "DESTRUYO LA PILA");
 	}
 
 }
